b12b945bad00fe1266abe16b3d9e92000e2ef6e6
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2015-06-09 19:07:00 +0000

    additional error handling

--- MCCCS/src/workflow/CreateCircularGradientImage.java
+++ MCCCS/src/workflow/CreateCircularGradientImage.java
@@ -10,14 +10,13 @@ import de.ipk.ag_ba.image.structures.Image;
  * Creates an circular gradient image (tiff float), the most far point from the
  * center has value 0, the center point has value 1. The dimensions of the
  * target image are determined from the given input (template) image.
- * 
  * input: filename output: filename for gradient image (should have tiff
  * extension)
  * 
  * @author klukas
  */
 public class CreateCircularGradientImage {
-
+	
 	public static void main(String[] args) throws Exception {
 		{
 			new Settings(true);
@@ -28,6 +27,11 @@ public class CreateCircularGradientImage {
 			System.exit(1);
 		} else {
 			File ff = new File(args[0]);
+			if (!ff.exists()) {
+				System.err.println("File input template '" + ff.getName()
+						+ "' could not be found! Return Code 2");
+				System.exit(2);
+			}
 			Image img = new Image(FileSystemHandler.getURL(ff));
 			int w = img.getWidth();
 			int h = img.getHeight();

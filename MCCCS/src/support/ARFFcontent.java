4220a630d9d957e40d294c4b3d926b97a1c1c0c9
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2016-01-23 19:44:15 +0000

    memory efficient arff file merging

--- MCCCS/src/support/ARFFcontent.java
+++ MCCCS/src/support/ARFFcontent.java
@@ -1,10 +1,15 @@
 package support;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.webstart.TextFile;
 
@@ -15,11 +20,12 @@ public class ARFFcontent {
 	
 	private String relationName = "";
 	private List<String> headerLines = new ArrayList<>();
-	private ArrayList<ArrayList<String>> columnData = new ArrayList<>();
+	private ArrayList<File> columnData = new ArrayList<File>();
+	private ArrayList<Integer> columnDataLineCount = new ArrayList<Integer>();
 	
 	public void appendColumnData(File f) throws IOException {
 		TextFile tf = new TextFile(f);
-		ArrayList<String> dataLines = new ArrayList<>();
+		TextFile dataLines = new TextFile();
 		boolean header = true;
 		for (String l : tf) {
 			if (header) {
@@ -42,14 +48,18 @@ public class ARFFcontent {
 					dataLines.add(l);
 			}
 		}
-		columnData.add(dataLines);
+		File tempF = File.createTempFile("iccs_", ".txt");
+		tempF.deleteOnExit();
+		dataLines.write(tempF);
+		columnData.add(tempF);
+		columnDataLineCount.add(dataLines.size());
 	}
 	
 	public void writeTo(File file, HashSet<Integer> removeColumns) throws IOException {
-		TextFile tf = new TextFile();
+		FileWriter tf = new FileWriter(file);
 		
-		tf.add("%");
-		tf.add("@relation '" + relationName + "'");
+		tf.write("%" + System.lineSeparator());
+		tf.write("@relation '" + relationName + "'" + System.lineSeparator());
 		
 		{
 			int colIdx = 0;
@@ -57,26 +67,44 @@ public class ARFFcontent {
 				if (hl.startsWith("@attribute")) {
 					colIdx++;
 					if (!removeColumns.contains(colIdx))
-						tf.add(hl);
+						tf.write(hl + System.lineSeparator());
 				} else
-					tf.add(hl);
+					tf.write(hl + System.lineSeparator());
 			}
 		}
 		
 		int maxLines = 0;
-		for (ArrayList<String> dl : columnData)
-			if (dl.size() > maxLines)
-				maxLines = dl.size();
+		for (Integer lineCount : columnDataLineCount)
+			if (lineCount > maxLines)
+				maxLines = lineCount;
 		
-		tf.add("@data");
+		tf.write("@data" + System.lineSeparator());
+		
+		LinkedHashMap<File, Stream<String>> file2stream = new LinkedHashMap<>();
+		LinkedHashMap<File, Iterator<String>> file2input = new LinkedHashMap<>();
 		
 		for (int line = 0; line < maxLines; line++) {
 			StringBuilder sb = new StringBuilder();
-			for (ArrayList<String> cd : columnData) {
+			
+			for (File f : file2input.keySet()) {
+				if (file2input.get(f) != null && !file2input.get(f).hasNext()) {
+					file2stream.get(f).close();
+					file2stream.remove(f);
+					file2input.remove(f);
+				}
+				if (file2input.get(f) == null) {
+					file2stream.put(f, Files.lines(f.toPath()));
+					file2input.put(f, file2stream.get(f).iterator());
+				}
+				Iterator<String> s = file2input.get(f);
+				if (s == null || !s.hasNext()) {
+					s = Files.lines(f.toPath()).iterator();
+				}
 				if (sb.length() > 0)
 					sb.append(",");
-				sb.append(cd.get(line % cd.size()));
+				sb.append(s.next());
 			}
+			
 			String val = sb.toString();
 			if (removeColumns.size() > 0) {
 				String[] valArr = val.split(",");
@@ -90,11 +118,14 @@ public class ARFFcontent {
 				}
 				val = sb.toString();
 			}
-			tf.add(val);
+			tf.write(val + System.lineSeparator());
 		}
 		
-		tf.add("%");
+		for (File f : file2stream.keySet())
+			file2stream.get(f).close();
 		
-		tf.write(file);
+		tf.write("%" + System.lineSeparator());
+		
+		tf.close();
 	}
 }

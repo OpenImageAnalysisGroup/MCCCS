6c1bee1c1123bf11d765044a146c86a3fde12f9f
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2016-12-30 23:07:45 +0100

    changes to make docker integration and example work

--- MCCCS/main/example_scripts/example_RGB_segmentation/prepare.sh
+++ MCCCS/main/example_scripts/example_RGB_segmentation/prepare.sh
@@ -1,8 +1,11 @@
-APPPATH=$(realpath $(realpath $1)/..)
-export JARLIST=$(find $APPPATH/lib | grep jar$ | paste -sd ":" -)
-export MBP="$APPPATH/mcccs.jar:$JARLIST"
+APPPATH=$(realpath $1)
+#export MBP="$APPPATH/lib/iap.jar:$APPPATH/mcccs.jar"
+LIBPATH=$(realpath $1/..)
+export JARLIST=$(find $LIBPATH/lib | grep jar$ | paste -sd ":" -)
+export MBP="$JARLIST:$LIBPATH/mcccs.jar"
 # avoid parallel warning message, by setting shell variable
 export SHELL=/bin/bash
+
 if [[ "$(uname)" == CYGWIN* ]]
 then
 	MBP=$(cygpath -mp $MBP)
@@ -17,16 +20,12 @@ rm -f diseaseClassifier.data
 rm -f fgbg.model
 #echo "mcccs jar:" $MBP
 if [ "$(uname)" == "Darwin" ]; then
-	export JAVA="java -Xmx5g -Dapple.awt.UIElement=true -cp $MBP workflow"
+	export JAVA="java -Dapple.awt.UIElement=true -cp $MBP workflow"
 else
-	export JAVA="java -Xmx5g -cp $MBP workflow"
+	export JAVA="java -cp $MBP workflow"
 fi 
-WEKAJAR=$APPPATH/lib/weka.jar
-if [[ "$(uname)" == CYGWIN* ]]
-then
-	WEKAJAR=$(cygpath -wp $WEKAJAR)
-fi
-export WEKA="java -Xmx5g -cp $WEKAJAR"
+
+export WEKA="java -cp $JARLIST"
 
 NPROCS=1
 OS=$(uname -s)
@@ -43,24 +42,24 @@ if [[ "$(uname)" == CYGWIN* ]]
 then
 	NPROCS=$(wmic cpu get NumberOfLogicalProcessors | sed -n -e '2{p;q}' | sed ':a;N;$!ba;y/\n/ /' | sed 's/ //g')
 fi
-echo "Requested Operation Mode: $3"
+echo "Requested operation mode: $3"
 if [ "$3" == "s" ];
 then
 	NPROCS=1
-	echo "Operation Mode: single-threaded"
+	echo "Operation mode: single-threaded"
 else
 if [ "$3" == "h" ];
 then
-	NPROCS=2
-	echo "Operation Mode: utilizing half number of CPUs ($NPROCS)"
+	NPROCS=$(($NPROCS/2))
+	echo "Operation mode: utilize half of the CPU cores ($NPROCS)"
 else
-	echo "Operation Mode: multi-threaded ($NPROCS CPUs detected)"
+	echo "Operation mode: utilize all CPU cores ($NPROCS detected)"
 fi
 fi
 
 export SYSTEM_cpu_n=$NPROCS
 
 #if parallel is not installed use xargs instead:
-par="parallel --gnu -j $NPROCS"
+par="parallel --halt 2 -u --gnu -j $NPROCS"
 type parallel >/dev/null 2>&1 || par="xargs -n1 -P$NPROCS -I{}" 
 echo "Parallel command: $par"

bf7db497f531952a9df45eaf4d0b21b40867cbc8
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2016-12-30 23:09:07 +0100

    changes to make docker integration and example work

--- MCCCS/main/example_scripts/example_RGB_segmentation/process_segmentation_example_1_classification.sh
+++ MCCCS/main/example_scripts/example_RGB_segmentation/process_segmentation_example_1_classification.sh
@@ -7,9 +7,9 @@ echo "°                                                                   °"
 echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
 echo "°                                                                   °"
 echo "°          V1.0 developed in 2015, 2016                             °"
-echo "°          by the following members of the                          °"
+echo "°          by the following members of the Research Group           °"
 echo "°                                                                   °"
-echo "°          - OPEN IMAGE ANALYSIS GROUP -                            °"
+echo "°          - IMAGE ANALYSIS at IPK -                                °"
 echo "°                                                                   °"
 echo "°          Jean-Michel Pape and                                     °"
 echo "°          Dr. Christian Klukas (Head of group)                     °"
@@ -20,15 +20,40 @@ echo "°              !! Script will stop in case of error. !!             °"
 echo "°           !!  Last output is READY in case of no error !!         °"
 echo "°                                                                   °"
 echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
+# stop script in case of error
 set -e
-chmod +x *.sh
-bash ./train_FGBG_CVPPP.sh .. A1_training m A1
-bash ./predict_CVPPP.sh .. A1_prediction A1_training/fgbg.model m A1
+START_SEC=$(date +%s)
+bash ./train_FGBG_CVPPP.sh .. A1_training h A1
+END_ALL=$(date +%s)
+DIFF_T=$(echo "$END_ALL - $START_SEC" | bc)
 
-bash ./train_FGBG_CVPPP.sh .. A2_training m A2
-bash ./predict_CVPPP.sh .. A2_prediction A2_training/fgbg.model m A2
+START_SEC=$(date +%s)
+bash ./predict_CVPPP.sh .. A1_prediction h A1_training A1
+END_ALL=$(date +%s)
+DIFF_P=$(echo "$END_ALL - $START_SEC" | bc)
 
-bash ./train_FGBG_CVPPP.sh .. A3_training m A3
-bash ./predict_CVPPP.sh .. A3_prediction A3_training/fgbg.model m A3
+START_SEC=$(date +%s)
+bash ./train_FGBG_CVPPP.sh .. A2_training h A2
+END_ALL=$(date +%s)
+DIFF_TT=$(echo "$END_ALL - $START_SEC" | bc)
 
+START_SEC=$(date +%s)
+bash ./predict_CVPPP.sh .. A2_prediction h A2_training A2
+END_ALL=$(date +%s)
+DIFF_PP=$(echo "$END_ALL - $START_SEC" | bc)
 
+START_SEC=$(date +%s)
+bash ./train_FGBG_CVPPP.sh .. A3_training h A3
+END_ALL=$(date +%s)
+DIFF_TTT=$(echo "$END_ALL - $START_SEC" | bc)
+
+START_SEC=$(date +%s)
+bash ./predict_CVPPP.sh .. A3_prediction h A3_training A3
+END_ALL=$(date +%s)
+DIFF_PPP=$(echo "$END_ALL - $START_SEC" | bc)
+
+echo "$DIFF_T || $DIFF_P"
+echo "$DIFF_TT || $DIFF_PP"
+echo "Time training: $DIFF_TTT || time prdiction: $DIFF_PPP"
+echo "A1_t: ""$DIFF_T"" A2_t: ""$DIFF_TT"" A3_t: ""$DIFF_TTT"
+echo "A1_p: ""$DIFF_P"" A2_p: ""$DIFF_PP"" A3_p: ""$DIFF_PPP"

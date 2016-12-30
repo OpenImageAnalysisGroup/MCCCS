c0d260b6a11afb432019bbaf4051d570d87dde82
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2016-12-30 23:06:18 +0100

    changes to make docker integration and example work

--- MCCCS/main/example_scripts/example_RGB_segmentation/predict_folder.sh
+++ MCCCS/main/example_scripts/example_RGB_segmentation/predict_folder.sh
@@ -7,7 +7,7 @@ echo -n "a"
 rm -f ${dir}/channel_*
 rm -f ${dir}/*.arff
 rm -f ${dir}/*_quantified.csv
-rm -f ${dir}/foreground.png
+rm -f ${dir}/foreground*
 $JAVA.SplitRGB ${dir}/*rgb*
 
 #apply filter
@@ -19,8 +19,8 @@ $JAVA.RGB2LAB ${dir}/*rgb_r.tif ${dir}/*rgb_g.tif ${dir}/*rgb_b.tif 8
 echo -n "c"
 for img in ${dir}/channel*;
 do
-$JAVA.FILTER ${img} ${img} 3 3 BLUR
-$JAVA.FILTER ${img} ${img} 4 4 MEDIAN
+	$JAVA.FILTER ${img} ${img} 3 3 BLUR
+	$JAVA.FILTER ${img} ${img} 4 4 MEDIAN
 done
 
 echo -n "d"
@@ -38,8 +38,14 @@ rm "${dir}/fgbgresult.tif"
 echo -n "g"
 cp "${dir}/foreground.png" "${dir}/foreground_cluster.png"
 $JAVA.Quantify 0 ${dir}/foreground
-rm "${dir}/foreground_cluster.png"
+#rm "${dir}/foreground_cluster.png"
 cat ${dir}/*_quantified.csv >> all_prediction_results.csv
+
+if [ -f ${dir}/mask_2.png ];
+then
+	echo -n "h"
+	$JAVA.CreateDiffImage ${dir}/mask_2.png ${dir}/foreground.png ${dir}/foreground_diff.png
+fi
 
 rm -f ${dir}/channel_*
 rm -f ${dir}/*.arff

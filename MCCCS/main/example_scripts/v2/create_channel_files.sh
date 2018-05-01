266d91defd4e83d193a0442421acaeb554166cc1
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2018-05-01 22:59:01 +0200

    intermediate changes

--- MCCCS/main/example_scripts/v2/create_channel_files.sh
+++ MCCCS/main/example_scripts/v2/create_channel_files.sh
@@ -7,14 +7,15 @@ echo -n "[${dir}]"
 TIME_START=$(date +%s)
 TMP=$(mktemp -d)
 
-# create pipes
-mkfifo ${TMP}/p_rgb1_r.tif ${TMP}/p_rgb1_g.tif ${TMP}/p_rgb1_b.tif
-mkfifo ${TMP}/p_rgb2_r.tif ${TMP}/p_rgb2_g.tif ${TMP}/p_rgb2_b.tif
-mkfifo ${TMP}/p_rgb3_r.tif ${TMP}/p_rgb3_g.tif ${TMP}/p_rgb3_b.tif
+# create pipes (rgb output for three color conversions)
+mkfifo ${TMP}/p_rgb1_r.tif ${TMP}/p_rgb1_g.tif ${TMP}/p_rgb1_b.tif # inp for rgb2hsv
+mkfifo ${TMP}/p_rgb2_r.tif ${TMP}/p_rgb2_g.tif ${TMP}/p_rgb2_b.tif # inp for rgb2xyz
+mkfifo ${TMP}/p_rgb3_r.tif ${TMP}/p_rgb3_g.tif ${TMP}/p_rgb3_b.tif # inp for rgb2lab
 
-mkfifo ${TMP}/p_hsv_h.tif ${TMP}/p_hsv_s.tif ${TMP}/p_hsv_v.tif
-mkfifo ${TMP}/p_lab_l.tif ${TMP}/p_lab_a.tif ${TMP}/p_lab_b.tif
-mkfifo ${TMP}/p_xyz_x.tif ${TMP}/p_xyz_y.tif ${TMP}/p_xyz_z.tif
+# create pipes (output of conversions for two filter operations)
+mkfifo ${TMP}/p_hsv1_h.tif ${TMP}/p_hsv1_s.tif ${TMP}/p_hsv1_v.tif		${TMP}/p_hsv2_h.tif ${TMP}/p_hsv2_s.tif ${TMP}/p_hsv2_v.tif # hsv inp for blur and median
+mkfifo ${TMP}/p_xyz1_x.tif ${TMP}/p_xyz1_y.tif ${TMP}/p_xyz1_z.tif		${TMP}/p_xyz2_x.tif ${TMP}/p_xyz2_y.tif ${TMP}/p_xyz2_z.tif # lab inp for blur and median
+mkfifo ${TMP}/p_lab1_l.tif ${TMP}/p_lab1_a.tif ${TMP}/p_lab1_b.tif		${TMP}/p_lab2_l.tif ${TMP}/p_lab2_a.tif ${TMP}/p_lab2_b.tif # xyz inp for blur and median
 
 # set-up processing pipeline (tree)
 
@@ -26,12 +27,15 @@ mkfifo ${TMP}/p_xyz_x.tif ${TMP}/p_xyz_y.tif ${TMP}/p_xyz_z.tif
 	done
 
 	# start rgb to hsv/xyz/lab conversion in background (wait for r/g/b input)
-	$JAVA.RGB2HSV -rgb_r ${TMP}/p_rgb1_r.tif -rgb_g ${TMP}/p_rgb1_g.tif -rgb_b ${TMP}/p_rgb1_b.tif -b 8 \
-			 -hsv_h ${TMP}/p_hsv_h.tif -hsv_s ${TMP}/p_hsv_s.tif -hsv_v ${TMP}/p_hsv_v.tif &
-	$JAVA.RGB2XYZ -rgb_r ${TMP}/p_rgb2_r.tif -rgb_g ${TMP}/p_rgb2_g.tif -rgb_b ${TMP}/p_rgb2_b.tif -b 8 \
-			 -xyz_x ${TMP}/p_xyz_x.tif -xyz_y ${TMP}/p_xyz_y.tif -xyz_z ${TMP}/p_xyz_z.tif &
-	$JAVA.RGB2LAB -rgb_r ${TMP}/p_rgb3_r.tif -rgb_g ${TMP}/p_rgb3_g.tif -rgb_b ${TMP}/p_rgb3_b.tif -b 8 \
-			 -lab_l ${TMP}/p_lab_l.tif -lab_a ${TMP}/p_lab_a.tif -lab_b ${TMP}/p_lab_b.tif &
+	$JAVA.RGB2HSV \
+			-rgb_r ${TMP}/p_rgb1_r.tif -rgb_g ${TMP}/p_rgb1_g.tif -rgb_b ${TMP}/p_rgb1_b.tif -b 8 \
+			-hsv_h >(tee ${TMP}/p_hsv1_h.tif ${TMP}/p_hsv2_h.tif) -hsv_s >(tee ${TMP}/p_hsv1_s.tif ${TMP}/p_hsv2_s.tif) -hsv_v >(${TMP}/p_hsv1_v.tif ${TMP}/p_hsv2_v.tif) &
+	$JAVA.RGB2XYZ \
+			-rgb_r ${TMP}/p_rgb2_r.tif -rgb_g ${TMP}/p_rgb2_g.tif -rgb_b ${TMP}/p_rgb2_b.tif -b 8 \
+			-xyz_x >(${TMP}/p_xyz1_x.tif ${TMP}/p_xyz2_x.tif) -xyz_y >(${TMP}/p_xyz1_y.tif ${TMP}/p_xyz2_y.tif) -xyz_z >(tee ${TMP}/p_xyz1_z.tif ${TMP}/p_xyz2_z.tif) &
+	$JAVA.RGB2LAB \
+			-rgb_r ${TMP}/p_rgb3_r.tif -rgb_g ${TMP}/p_rgb3_g.tif -rgb_b ${TMP}/p_rgb3_b.tif -b 8 \
+			-lab_l >(tee ${TMP}/p_lab1_l.tif ${TMP}/p_lab2_l.tif) -lab_a >(tee ${TMP}/p_lab1_a.tif ${TMP}/p_lab2_a.tif) -lab_b >(tee ${TMP}/p_lab1_b.tif ${TMP}/p_lab2_b.tif) &
 
 # split RGB to R/G/B, fill pipeline with data
 python3 extract_tiff_channels.py \
@@ -61,6 +65,6 @@ TIME_DIFF=$(echo "${TIME_END} - ${TIME_START}" | bc)
 if [ "$NPROCS" == "1" ]
 then
 	echo "[${TIME_DIFF} seconds]"
-	else
+else
 	echo -n "[finished ${dir} in ${TIME_DIFF} sec.]"
 fi
